7919aef9a30bcc562c38eeccf18398bdbcd445a9
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-08-21 19:52:15 +0200

    encodes a run of length one

--- lib/runlength.ex
+++ lib/runlength.ex
@@ -1,5 +1,8 @@
 defmodule Runlength do
   def encode(string) do
-    ""
+    case string |> String.codepoints do
+      [] -> ""
+      [char | []] -> "1#{char}" 
+    end
   end
 end
--- test/runlength_test.exs
+++ test/runlength_test.exs
@@ -5,4 +5,8 @@ defmodule RunlengthTest do
   test "does nothing for empty string" do
     assert Runlength.encode("") == ""
   end
+
+  test "encodes a run of length one" do
+    assert Runlength.encode("J") == "1J"
+  end
 end
