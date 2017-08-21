5b10f3a6a00a7325e647c6d5491a6965a41a5f19
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-08-21 19:25:46 +0200

    initial project setup

---
+++ lib/runlength.ex
@@ -0,0 +1,18 @@
+defmodule Runlength do
+  @moduledoc """
+  Documentation for Runlength.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Runlength.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/runlength_test.exs
@@ -0,0 +1,8 @@
+defmodule RunlengthTest do
+  use ExUnit.Case
+  doctest Runlength
+
+  test "greets the world" do
+    assert Runlength.hello() == :world
+  end
+end
